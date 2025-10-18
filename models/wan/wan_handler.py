0cf7117c1bcb02bcbc13b6ba7d0a50e6739c8197
Gunther Schulz <kunde@guntherschulz.de>
2025-10-19 00:13:44 +0200

    Rename LCM scheduler display name from 'LCM (RectifiedFlow)' to 'LCM'
    - Consistent with other sampler names (euler, dpm++, unipc)
    - RectifiedFlow is just the standard linear sigma schedule all flow matchers use
    - The differentiator is the LCM stepping logic, not the schedule

--- models/wan/wan_handler.py
+++ models/wan/wan_handler.py
@@ -180,7 +180,7 @@ class family_handler():
                             ("euler", "euler"),
                             ("dpm++", "dpm++"),
                             ("flowmatch causvid", "causvid"),
-                            ("LCM (RectifiedFlow)", "lcm"), ]
+                            ("LCM", "lcm"), ]
         })
 
 
